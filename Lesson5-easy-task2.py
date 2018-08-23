b50f0e28232b6d37f54d49c0f81157a5f9597acd
sesazhin <sesazhin@cisco.com>
2018-08-23 18:25:23 +0300

    Changed code for task2 in Lesson5 so that it shows only directories, not just all files (os.path.isdir(path) is used for it)

--- Lesson5-easy-task2.py
+++ Lesson5-easy-task2.py
@@ -6,11 +6,16 @@ import os
 
 def list_dir():
 
-    dir_full_path = os.path.join(os.getcwd())
+    curr_dir = os.getcwd()
 
     try:
-        for elem in os.listdir(dir_full_path):
-            print(elem)
+
+        for elem in os.listdir(curr_dir):
+
+            dir_full_path = os.path.join(os.getcwd(),elem)
+
+            if os.path.isdir(dir_full_path):
+                print("directory = ",dir_full_path)
     except:
         print("Невозможно прочитать содержимое директории")
 
